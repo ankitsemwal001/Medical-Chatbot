9c656e960d1375c9cb464e7537f5c3a61a0bfd2a
ankitsemwal001 <ankitsemwal0884@gmail.com>
2025-11-04 22:18:28 +0530

    Updated files

--- Medical_Chatbot/mediBot/views.py
+++ Medical_Chatbot/mediBot/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
 def home(request):
-    return redirect('adminLogin')
+    return render(request,'Html/home.html')
+
 def adminLogin(request):
     return render(request,'Html/adminLogin.html')
